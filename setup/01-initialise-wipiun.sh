c5c8c0ea7ff103cefeb8a82938c011d94483b7f2
papalozarou <github@lozworld.com>
2023-11-06 20:44:08 +0000

    Added stub setup files with first stab at 01

---
+++ setup/01-initialise-wipiun.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+#-------------------------------------------------------------------------------
+# Initialises the setup by:
+#
+# 1. updating and upgrading packages; 
+# 2. checking for a config directory and file; and
+# 3. preparing the service files by removing the ".example" postfix.
+# 
+# N.B.
+# This script needs to be run as "sudo".
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Imported variables.
+#-------------------------------------------------------------------------------
+. ./linshafun/setup.var
+
+#-------------------------------------------------------------------------------
+# Imported shared functions.
+#-------------------------------------------------------------------------------
+. ./linshafun/comments.sh
+# . ./linshafun/docker-env-variables.sh
+# . ./linshafun/docker-images.sh
+# . ./linshafun/docker-services.sh
+. ./linshafun/files-directories.sh
+# . ./linshafun/firewall.sh
+# . ./linshafun/host-env-variables.sh
+# . ./linshafun/network.sh
+. ./linshafun/ownership-permissions.sh
+. ./linshafun/packages.sh
+# . ./linshafun/services.sh
+. ./linshafun/setup-config.sh
+. ./linshafun/setup.sh
+# . ./linshafun/ssh-keys.sh
+# . ./linshafun/text.sh
+# . ./linshafun/user-input.sh
+
+#-------------------------------------------------------------------------------
+# Config key variable.
+#-------------------------------------------------------------------------------
+CONFIG_KEY='initialisedWipiun'
+
+#-------------------------------------------------------------------------------
+# Executes the main functions of the script.
+#-------------------------------------------------------------------------------
+mainScript () {
+  updateUpgrade
+
+  checkForSetupConfigFileAndDir
+
+  removePostfixFromFiles
+}
+
+#-------------------------------------------------------------------------------
+# Run the script.
+#-------------------------------------------------------------------------------
+initialiseScript "$CONFIG_KEY"
+mainScript
+finaliseScript "$CONFIG_KEY"
