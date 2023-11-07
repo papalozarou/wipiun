1317ce4e3f48bad6e3552dbfb98d50e31295b7af
papalozarou <github@lozworld.com>
2023-11-07 17:19:57 +0000

    Added function to generate server port

--- setup/02-configure-wipiun.sh
+++ setup/02-configure-wipiun.sh
@@ -3,10 +3,11 @@
 #-------------------------------------------------------------------------------
 # Sets up wipiun by:
 #
-# 1. asking the user for a list of wireguard client machines;
-# 2. checking and setting the container and VPN network IP addresses;
-# 3. adding the wireguard port to UFW; and
-# 3. building the wireguard and unbound images.
+# 1. generating and setting the server port for wireguard;
+# 2. asking the user for a list of wireguard client machines;
+# 3. checking and setting the container and VPN network IP addresses;
+# 4. adding the wireguard port to UFW; and
+# 5. building the wireguard and unbound images.
 # 
 # N.B.
 # This script needs to be run as "sudo".
@@ -28,7 +29,7 @@
 # . ../linshafun/files-directories.sh
 . ../linshafun/firewall.sh
 # . ../linshafun/host-env-variables.sh
-# . ../linshafun/network.sh
+. ../linshafun/network.sh
 . ../linshafun/ownership-permissions.sh
 # . ../linshafun/packages.sh
 . ../linshafun/services.sh
@@ -45,10 +46,27 @@ CONFIG_KEY='configuredWipiun'
 
 #-------------------------------------------------------------------------------
 # Get a comma separated list of wireguard clients.
-#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------                    
 getWireguardClients () {
   promptForUserInput 'Please enter a comma separeted list of your wireguard clients.' 'This list must be comma separated.'
   WGD_CLIENTS="$(getUserInput)"
+}
+
+#-------------------------------------------------------------------------------
+# Sets the server port for wireguard. Checks against rtorrent and plex ports to 
+# see if the generated port is the same. If so re-run this function and if not 
+# replace the port number in the ".env" file.
+#-------------------------------------------------------------------------------
+setWireguardServerPort () {
+  local WGD_SERVER_PORT="$(generateAndCheckPort "ssh")"
+  local RTT_PORT_CHECK_TF="$(checkAgainstExistingPortNumber "rtt")"
+  local PLEX_PORT="32400"
+
+  if [ "$RTT_PORT_CHECK_TF" = true ] || [ "$WGD_SERVER_PORT" = "$PLX_PORT" ]; then
+    setWireguardServerPort
+  fi
+
+  setDockerEnvVariables "$DOCKER_ENV_FILE" 'C_WGD_SERVER_PORT' "$WGD_SERVER_PORT"
 }
 
 #-------------------------------------------------------------------------------
