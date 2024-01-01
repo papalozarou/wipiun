5e3a2e7c194991e052e9362a904b248a55684b15
papalozarou <github@lozworld.com>
2024-01-01 14:25:18 +0000

    renamed 04 to 05

---
+++ setup/05-start-containers.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+#-------------------------------------------------------------------------------
+# Starts all the containers.
+# 
+# N.B.
+# This script needs to be run as "sudo".
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Imported shared variables.
+#-------------------------------------------------------------------------------
+. ../linshafun/setup.var
+
+#-------------------------------------------------------------------------------
+# Imported shared functions.
+#-------------------------------------------------------------------------------
+. ../linshafun/comments.sh
+# . ../linshafun/docker-env-variables.sh
+# . ../linshafun/docker-images.sh
+# . ../linshafun/docker-services.sh
+# . ../linshafun/files-directories.sh
+# . ../linshafun/firewall.sh
+# . ../linshafun/host-env-variables.sh
+# . ../linshafun/network.sh
+. ../linshafun/ownership-permissions.sh
+# . ../linshafun/packages.sh
+# . ../linshafun/services.sh
+. ../linshafun/setup-config.sh
+. ../linshafun/setup.sh
+# . ../linshafun/ssh-keys.sh
+# . ../linshafun/text.sh
+# . ../linshafun/user-input.sh
+
+#-------------------------------------------------------------------------------
+# Config key.
+#-------------------------------------------------------------------------------
+CONFIG_KEY='startedContainers'
+
+#-------------------------------------------------------------------------------
+# Executes the main functions of the script.
+#-------------------------------------------------------------------------------
+mainScript () {
+  echoComment 'Starting all containers.'
+  docker compose up -d
+
+  echoServiceWait 'all services' 'start' '45'
+
+  echoSeparator
+  docker compose ps -a
+  echoSeparator
+  
+  echoComment 'All containers should now be started.'
+}
+
+#-------------------------------------------------------------------------------
+# Run the script.
+#-------------------------------------------------------------------------------
+initialiseScript "$CONFIG_KEY"
+mainScript
+finaliseScript "$CONFIG_KEY"
